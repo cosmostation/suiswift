80a346aaa118bd6ad1199b79a6a104d5de89e6a5
yongjoo <yongjoo@wannabit.io>
2023-01-13 23:22:34 +0900

    add funcs

--- Sources/SuiSwift/SuiClient.swift
+++ Sources/SuiSwift/SuiClient.swift
@@ -11,9 +11,27 @@ import web3swift
 import SwiftyJSON
 
 open class SuiClient {
+    
+    var rpc_endpoint: String!
+    var faucet_endpoint: String!
+    
     public static let shared = SuiClient()
     
-    public init() {}
+    public init() { }
+    
+    public func setConfig(_ chainType: ChainType) {
+        switch chainType {
+        case .local:
+            rpc_endpoint = SuiConstant.LOCAL_RPC_URL
+            faucet_endpoint = SuiConstant.LOCAL_FAUCET_URL
+        case .devnet:
+            rpc_endpoint = SuiConstant.DEV_RPC_URL
+            faucet_endpoint = SuiConstant.DEV_FAUCET_URL
+        case .testnet:
+            rpc_endpoint = SuiConstant.TEST_RPC_URL
+            faucet_endpoint = SuiConstant.TEST_FAUCET_URL
+        }
+    }
     
     public func generateMnemonic() -> String? {
         return try? BIP39.generateMnemonics(bitsOfEntropy: 128)
@@ -24,7 +42,7 @@ open class SuiClient {
     }
     
     public func faucet(_ address: String) {
-        AF.request("https://faucet.devnet.sui.io/gas",
+        AF.request(faucet_endpoint,
                    method: .post,
                    parameters: FaucetRequest(FixedAmountRequest: FixedAmountRequest(recipient: address)),
                    encoder: JSONParameterEncoder.default).response { response in
@@ -37,74 +55,61 @@ open class SuiClient {
         return (SuiKey.getPubKey(mnemonic), SuiKey.sign(seedKey, txBytes))
     }
     
+    public func getSuiSystemstate(_ listener: @escaping (JSON?) -> Void) {
+        let params = JsonRpcRequest("sui_getSuiSystemState", JSON())
+        SuiRequest(params, listener)
+    }
+    
+    public func getTotalSupply(_ coinType: String, _ listener: @escaping (JSON?) -> Void) {
+        let params = JsonRpcRequest("sui_getTotalSupply", JSON())
+        SuiRequest(params, listener)
+    }
+    
+    public func getAllBalances(_ address: String, _ listener: @escaping (JSON?) -> Void) {
+        let params = JsonRpcRequest("sui_getAllBalances", JSON(arrayLiteral: address))
+        SuiRequest(params, listener)
+    }
+    
+    public func getAllBalance(_ address: String, _ coinType: String, _ listener: @escaping (JSON?) -> Void) {
+        let params = JsonRpcRequest("sui_getBalance", JSON(arrayLiteral: address, coinType))
+        SuiRequest(params, listener)
+    }
+    
+    public func getAllCoins(_ owner: String, _ cursor: String? = nil, _ limit: Int? = nil, _ listener: @escaping (JSON?) -> Void) {
+        let params = JsonRpcRequest("sui_getAllCoins", JSON(arrayLiteral: owner, cursor, limit))
+        SuiRequest(params, listener)
+    }
+    
+    public func getCoins(_ owner: String, _ coinType: String, _ cursor: String? = nil, _ limit: Int? = nil, _ listener: @escaping (JSON?) -> Void) {
+        let params = JsonRpcRequest("sui_getCoins", JSON(arrayLiteral: owner, cursor, limit))
+        SuiRequest(params, listener)
+    }
+    
+    public func getCoinMetadata(_ coinType: String, _ listener: @escaping (JSON?) -> Void) {
+        let params = JsonRpcRequest("sui_getCoinMetadata", JSON(arrayLiteral: coinType))
+        SuiRequest(params, listener)
+    }
+    
     public func getObjectsByOwner(_ address: String, _ listener: @escaping (JSON?) -> Void) {
         let params = JsonRpcRequest("sui_getObjectsOwnedByAddress", JSON(arrayLiteral: address))
-        AF.request(SuiConstant.RPC_URL,
-                   method: .post,
-                   parameters: params,
-                   encoder: JSONParameterEncoder.default).response { response in
-            switch response.result {
-            case .success(let value):
-                if let value = value, let response = try? JSONDecoder().decode(JsonRpcResponse.self, from: value) {
-                    listener(response.result)
-                }
-            case .failure(let error):
-                debugPrint(error)
-            }
-        }
+        SuiRequest(params, listener)
     }
     
     public func getTransactions(_ transactionQuery: [String: String], _ nextOffset: String? = nil, _ limit: Int? = nil, _ descending: Bool = false, _ listener: @escaping (JSON?) -> Void) {
         let params = JsonRpcRequest("sui_getTransactions", JSON(arrayLiteral: transactionQuery, nextOffset, limit, descending))
-        AF.request(SuiConstant.RPC_URL,
-                   method: .post,
-                   parameters: params,
-                   encoder: JSONParameterEncoder.default).response { response in
-            switch response.result {
-            case .success(let value):
-                if let value = value, let response = try? JSONDecoder().decode(JsonRpcResponse.self, from: value) {
-                    listener(response.result)
-                }
-            case .failure(let error):
-                debugPrint(error)
-            }
-        }
+        SuiRequest(params, listener)
     }
     
     public func getTransactionDetails(_ digests: [String], _ listener: @escaping (JSON?) -> Void) {
         let params = JsonRpcRequest("sui_getTransaction", JSON(digests))
-        AF.request(SuiConstant.RPC_URL,
-                   method: .post,
-                   parameters: params,
-                   encoder: JSONParameterEncoder.default).response { response in
-            switch response.result {
-            case .success(let value):
-                if let value = value, let response = try? JSONDecoder().decode(JsonRpcResponse.self, from: value) {
-                    listener(response.result)
-                }
-            case .failure(let error):
-                debugPrint(error)
-            }
-        }
+        SuiRequest(params, listener)
     }
     
     public func transferObject(_ objectId: String, _ receiver: String,
                                _ sender: String, _ gasBudget: Int = 100, _ amount: Int? = nil
                                , _ listener: @escaping (JSON?) -> Void) {
         let params = JsonRpcRequest("sui_transferSui", JSON(arrayLiteral: sender, objectId, gasBudget, receiver))
-        AF.request(SuiConstant.RPC_URL,
-                   method: .post,
-                   parameters: params,
-                   encoder: JSONParameterEncoder.default).response { response in
-            switch response.result {
-            case .success(let value):
-                if let value = value, let response = try? JSONDecoder().decode(JsonRpcResponse.self, from: value) {
-                    listener(response.result)
-                }
-            case .failure(let error):
-                debugPrint(error)
-            }
-        }
+        SuiRequest(params, listener)
     }
     
     public func executeTransaction(_ txBytes: Data, _ signedBytes: Data, _ pubKey: Data, _ listener: @escaping (JSON?) -> Void) {
@@ -113,7 +118,11 @@ open class SuiClient {
                                                                    signedBytes.base64EncodedString(),
                                                                    pubKey.base64EncodedString(),
                                                                    "WaitForLocalExecution"))
-        AF.request(SuiConstant.RPC_URL,
+        SuiRequest(params, listener)
+    }
+    
+    private func SuiRequest(_ params: JsonRpcRequest, _ listener: @escaping (JSON?) -> Void) {
+        AF.request(rpc_endpoint,
                    method: .post,
                    parameters: params,
                    encoder: JSONParameterEncoder.default).response { response in
@@ -123,6 +132,7 @@ open class SuiClient {
                     listener(response.result)
                 }
             case .failure(let error):
+                print("error ", error)
                 debugPrint(error)
             }
         }
