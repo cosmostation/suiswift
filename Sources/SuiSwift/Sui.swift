dc01df9f2f817de56aa1e75cf9a75a69bcc06c46
yongjoo <yongjoo@wannabit.io>
2023-04-11 20:09:31 +0900

    Update endpoint for testnet

--- Sources/SuiSwift/Sui.swift
+++ Sources/SuiSwift/Sui.swift
@@ -13,8 +13,7 @@ class SuiConstant {
     static let LOCAL_FAUCET_URL = "http://127.0.0.1:5003/gas"
     static let DEV_RPC_URL = "https://explorer-rpc.devnet.sui.io"
     static let DEV_FAUCET_URL = "https://faucet.devnet.sui.io/gas"
-//    static let TEST_RPC_URL = "https://fullnode.testnet.sui.io/"
-    static let TEST_RPC_URL = "https://explorer-rpc.testnet.sui.io"
+    static let TEST_RPC_URL = "https://rpc-sui-testnet.cosmostation.io"
     static let TEST_FAUCET_URL = "https://faucet.testnet.sui.io/gas"
     static let MAIN_RPC_URL = ""
     
