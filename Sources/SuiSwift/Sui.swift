bb16c9287de5bbd4dd6fa06b67bb595f43016f0d
soaryong-c <91711862+soaryong-c@users.noreply.github.com>
2023-04-14 23:14:36 +0900

    Update Sui.swift

--- Sources/SuiSwift/Sui.swift
+++ Sources/SuiSwift/Sui.swift
@@ -11,7 +11,7 @@ class SuiConstant {
     
     static let LOCAL_RPC_URL = "http://127.0.0.1:9000/"
     static let LOCAL_FAUCET_URL = "http://127.0.0.1:5003/gas"
-    static let DEV_RPC_URL = "https://explorer-rpc.devnet.sui.io"
+    static let DEV_RPC_URL = "https://sui-devnet-kr-1.cosmostation.io"
     static let DEV_FAUCET_URL = "https://faucet.devnet.sui.io/gas"
     static let TEST_RPC_URL = "https://rpc-sui-testnet.cosmostation.io"
     static let TEST_FAUCET_URL = "https://faucet.testnet.sui.io/gas"
